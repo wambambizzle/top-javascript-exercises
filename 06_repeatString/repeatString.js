cc464877fe8cf4ed9c99f9631b86dc97b14c974d
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-05 14:20:05 -0500

    RepeatString func implemented

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repeatingString = "";
+  for (let i = 0; i < num; i++) {
+    repeatingString += string;
+  }
 
+  return repeatingString;
 };
 
 // Do not edit below this line
