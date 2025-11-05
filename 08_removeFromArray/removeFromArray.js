f28f9c63f0f82b6bfbce7db8a45be0f8c236039b
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-05 17:03:39 -0500

    Make em all pass baybayyy

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, a, ...theArgs) {
+  let numberOccurrences = getNumberOccurrences(arr, a);
+
+  for (let i = 0; i < numberOccurrences; i++) {
+    removeValueForIndex(a);
+  }
+
+  for (let index = 0; index < theArgs.length; index++) {
+    const element = theArgs[index];
+    let indexToRemove = arr.indexOf(element);
+
+    if (indexToRemove >= 0) {
+      arr.splice(indexToRemove, 1);
+    }
+  }
+
+  return arr;
+
+  function removeValueForIndex(value) {
+    let indexToRemove = arr.indexOf(value);
+
+    if (indexToRemove >= 0) {
+      arr.splice(indexToRemove, 1);
+    }
+  }
 };
+
+function getNumberOccurrences(array, value) {
+  var count = 0;
+  array.forEach((v) => v === value && count++);
+  return count;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
