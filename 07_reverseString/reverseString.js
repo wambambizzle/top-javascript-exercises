eed8594e43de002d74f9486784c205b9faed95fa
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-05 14:37:08 -0500

    Get my reverse string on

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // let stringsArray = string.split("");
+  // let reversedArray = stringsArray.reverse();
+  // let reversedString = reversedArray.join("");
+  // console.log(reversedString);
 
+  let reversedString = string.split("").reverse().join("");
+
+  console.log(reversedString);
+
+  return reversedString;
 };
 
 // Do not edit below this line
