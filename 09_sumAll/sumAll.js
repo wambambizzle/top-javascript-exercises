41fab5cedec09b3ea838fe88480f7469b7e7cc19
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-05 17:49:03 -0500

    SUM IT ALL UP - yall aint tricking me back to back ..

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
 
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  let array = createNumberArrayLoop(a, b);
+  return array.reduce((sum, current) => sum + current, 0);
 };
+
+function createNumberArrayLoop(start, end) {
+  const arr = [];
+  if (start <= end) {
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i--) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
