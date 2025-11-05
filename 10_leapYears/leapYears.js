decb9ae84fb837b10524ee03ff3da6903dc92d5f
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-05 18:05:02 -0500

    Just dunked on leap year rulesssss

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isLeapYear = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isFourCentury = year % 400 === 0;
 
+  if (isCentury && !isFourCentury) {
+    return false;
+  }
+
+  if (isLeapYear && isCentury && isFourCentury) {
+    return true;
+  }
+
+  if (isLeapYear) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
