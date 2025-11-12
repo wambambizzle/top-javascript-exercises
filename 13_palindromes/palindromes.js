95dde5730e3fa94418dc6f627a59091275067f7d
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-12 17:18:06 -0500

    Get em all but twooooo!

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const endsWithPunctuation = !!word.match(/[.,:!?]$/);
 
+  let charArray = word.split(",");
+  let punctuation = "";
+
+  if (endsWithPunctuation) {
+    let punctuationBlock = charArray[charArray.length - 1];
+    let punctuationBlockArray = punctuationBlock.split("");
+    punctuation = punctuationBlockArray.pop();
+
+    let updateBlockArray = punctuationBlockArray.join("");
+    charArray[charArray.length - 1] = updateBlockArray;
+  }
+
+  const reversedArray = charArray.reverse();
+  let reversedWord = reversedArray.join(" ");
+
+  if (endsWithPunctuation) {
+    reversedWord += punctuation;
+  }
+
+  return reversedWord.toLowerCase() == word.toLowerCase();
 };
 
 // Do not edit below this line
