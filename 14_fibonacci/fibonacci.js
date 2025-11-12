efbcda0837095cb00de725fb599a19e4d31f9d83
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-12 18:20:53 -0500

    I love you stack overflowwww

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS"
+  }
 
+  if (typeof num === "string") {
+    num = Number(num)
+  }
+
+  if (num <= 1) {
+    return num;
+  }
+
+  let a = 0, b = 1;
+
+  for (let i = 2; i <= num; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b
 };
 
 // Do not edit below this line
