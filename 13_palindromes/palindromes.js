722160796227b15685f5b83d26ce1ad96f7ba5c8
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-12 17:59:45 -0500

    Bruh.. - they remove the damn punctuation !

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,26 +1,33 @@
-const palindromes = function (word) {
-  const endsWithPunctuation = !!word.match(/[.,:!?]$/);
+const palindromes = function (string) {
+  // const endsWithPunctuation = !!word.match(/[.,:!?]$/);
+  // let charArray = word.split(",");
+  // let punctuation = "";
+  // if (endsWithPunctuation) {
+  //   let punctuationBlock = charArray[charArray.length - 1];
+  //   let punctuationBlockArray = punctuationBlock.split("");
+  //   punctuation = punctuationBlockArray.pop();
+  //   let updateBlockArray = punctuationBlockArray.join("");
+  //   charArray[charArray.length - 1] = updateBlockArray;
+  // }
+  // const reversedArray = charArray.reverse();
+  // let reversedWord = reversedArray.join(" ");
+  // if (endsWithPunctuation) {
+  //   reversedWord += punctuation;
+  // }
+  // console.log(`### reversedWord: ${reversedWord}`);
+  // console.log(`### word: ${word}`);
+  // return reversedWord.toLowerCase() == word.toLowerCase();
 
-  let charArray = word.split(",");
-  let punctuation = "";
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
 
-  if (endsWithPunctuation) {
-    let punctuationBlock = charArray[charArray.length - 1];
-    let punctuationBlockArray = punctuationBlock.split("");
-    punctuation = punctuationBlockArray.pop();
+  const reversedString = cleanedString.split("").reverse().join("");
 
-    let updateBlockArray = punctuationBlockArray.join("");
-    charArray[charArray.length - 1] = updateBlockArray;
-  }
-
-  const reversedArray = charArray.reverse();
-  let reversedWord = reversedArray.join(" ");
-
-  if (endsWithPunctuation) {
-    reversedWord += punctuation;
-  }
-
-  return reversedWord.toLowerCase() == word.toLowerCase();
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
