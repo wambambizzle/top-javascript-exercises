39d9742e6171ed8389592c8aa0b663ba66449485
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-12 19:20:45 -0500

    GOTT EMMM

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson = people.filter((people) => people.yearOfDeath != null).reduce((oldest, current) => {
+    if (oldest.yearOfDeath == undefined && oldest.yearOfBirth > current.yearOfBirth) {
+      return current
+    }
 
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    return (currentAge > oldestAge ? current : oldest);
+  }, people[0]);
+
+  return oldestPerson
 };
 
 // Do not edit below this line
